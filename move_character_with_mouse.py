002fd285fd8625630a98b3f9c98cea3c2edc7c29
minsu <gnffk48@naver.com>
2023-09-30 02:28:53 +0900

    hand위치 랜덤

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,12 +1,14 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
-
+switch = True
 def handle_events():
     global running
     global x, y
@@ -14,11 +16,19 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
+        elif event.type != SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
     pass
+
+def hand_random():
+    global x,y, x1,y1, x2,y2
+    x2 = random.randint(0,TUK_WIDTH)
+    y2 = random.randint(0,TUK_HEIGHT)
+    x1,y2 = x, y
+    switch = False
 
 
 
@@ -29,12 +39,16 @@ hide_cursor()
 
 while running:
     clear_canvas()
+    if(switch):
+        hand_random()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(0, 0, 50, 52, x2, y2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
-    frame = (frame + 1) % 8
 
+    frame = (frame + 1) % 8
     handle_events()
+    delay(0.03)
 
 close_canvas()
 
